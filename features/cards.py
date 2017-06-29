a55110c0257a92eed2d0d3d386464250ebd276af
Mark Zeman <zemanmark@hotmail.com>
2017-06-29 21:42:39 +0200

    Fixed bug

--- features/cards.py
+++ features/cards.py
@@ -5,6 +5,8 @@ def get_card_url(command):
 
     for string in command:
         string.replace("'", "-")
-        card_name += '-' + string.lower()
+        if card_name != '':
+            card_name += '-'
+        card_name += string.lower()
 
     return "https://fiveringsdb.com/bundles/card_images/" + card_name + ".png"
