0cfc1d3223cb9673c7b7d73469abefca11875c91
Mark Zeman <zemanmark@hotmail.com>
2017-07-07 17:39:41 +0200

    Greatly improved card search, now validates that cards exist and does fuzzy matching to propose alternatives

--- features/cards.py
+++ features/cards.py
@@ -1,33 +1,71 @@
 import urllib
 import requests
+import json
+import datetime
+from fuzzywuzzy import process
 import logging
-
 
 logger = logging.getLogger('discord')
 
 
 def get_card_url(command):
-
     logger.info("Getting a card URL for " + str(command))
     card_name = ''
 
     for string in command:
-        string = urllib.parse.unquote(string)
-        string = string.replace("'", "-")
         if card_name != '':
-            card_name += '-'
+            card_name += ' '
         card_name += string.lower()
 
-    if validate_card(card_name):
+    valid_name, cards = validate_card_name(card_name)
+    if valid_name:
+        card_name = urllib.parse.unquote(card_name)
+        card_name = card_name.replace("'", "-")
+        card_name = card_name.replace(" ", "-")
         return "https://fiveringsdb.com/bundles/card_images/" + card_name + ".png"
     else:
-        return "I'm sorry, honourable samurai-san, but this card is not known."
+        message = ""
+        for card, _ in cards:
+            if message != "":
+                message += ", "
+            message += prettify_name(card)
+        return "I'm sorry, honourable samurai-san, but this card is not known. \n" + \
+               "Perhaps you meant one of these three? \n" + message
 
 
-def validate_card(card_name):
+def validate_card_name(card_name):
+    """Returns the card name or potential alternatives."""
 
-    r = requests.get("https://fiveringsdb.com/api/v1/cards/" + card_name)
-    if r.status_code != "200":
-        return False
+    with open('card_db.json', 'r') as json_data:
+        try:
+            db_records = json.load(json_data)
+        except json.decoder.JSONDecodeError:
+            db_records = {}
 
-    return True
+    if db_records == {} or datetime.datetime.strptime(db_records['last_updated'], "%Y-%m-%dT%H:%M:%S").day < datetime.datetime.utcnow().day:
+        r = requests.get("https://fiveringsdb.com/api/v1/cards")
+        request_data = r.json()
+
+        db_records['last_updated'] = request_data['last_updated'][:-6]
+        card_names = []
+        for card in request_data['records']:
+            card_names.append(card['name_canonical'])
+        db_records['cards'] = card_names
+        with open('card_db.json', 'w') as outfile:
+            json.dump(db_records, outfile)
+            logger.info('Saved new card_db to file')
+
+    if card_name in db_records['cards']:
+        return True, ""
+
+    potentials = process.extract(card_name, set(db_records['cards']), limit=3)
+    return False, potentials
+
+
+def prettify_name(cardname):
+    cardname = cardname.split(" ")
+    pretty = ""
+    for word in cardname:
+        pretty += word.capitalize() + " "
+
+    return pretty
