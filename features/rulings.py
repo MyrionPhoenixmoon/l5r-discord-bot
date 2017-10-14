00810e15b3f335cb73dbfe550997d29fa975ad49
Mark Z <myrion_the_first@hotmail.com>
2017-10-14 21:41:05 +0200

    Made bot more tolerant of missing data and enhanced rulings

--- features/rulings.py
+++ features/rulings.py
@@ -8,6 +8,9 @@ import logging
 
 logger = logging.getLogger('discord')
 
+DESCRIPTION_TEXT = 'Click the links for more information'
+TITLE_TEXT = 'Provided by fiveringsdb.com'
+
 
 def get_rulings(command):
     card_name = ''
@@ -31,32 +34,31 @@ def get_rulings(command):
         card_name = card_name.replace("!", "")
 
         logger.info("It's a valid card, fetching rulings now")
-        url = "https://api.fiveringsdb.com/cards/" + card_name + "/rulings"
+        url = "https://fiveringsdb.com/card/" + card_name
         response = requests.get("https://api.fiveringsdb.com/cards/" + card_name + "/rulings")
 
         entries = response.json()["records"]
         data['entries'], split_at = split_rulings(entries)
         data['url'] = url
 
-
         if split_at > 0:
-            em1 = discord.Embed(title="Rulings for " + prettify_name(data['card_name'] + " Part 1"), description='Rulings',
+            em1 = discord.Embed(title=TITLE_TEXT, description=DESCRIPTION_TEXT,
                                 colour=0xDEADBF, url=data['url'])
             for entry in data['entries'][0:split_at]:
-                em1.add_field(name=entry['source'], value=entry['text'])
-            em2 = discord.Embed(title="Rulings for " + prettify_name(data['card_name']) + " Part 2", description='Rulings',
+                em1.add_field(name=(entry['link'] if entry['link'] is not None else entry['source']), value=entry['text'])
+            em2 = discord.Embed(title=TITLE_TEXT, description=DESCRIPTION_TEXT,
                                 colour=0xDEADBF, url=data['url'])
             for entry in data['entries'][split_at:]:
-                em2.add_field(name=entry['source'], value=entry['text'])
-            em1.set_author(name="Miya Herald")
-            em2.set_author(name="Miya Herald")
+                em2.add_field(name=(entry['link'] if entry['link'] is not None else entry['source']), value=entry['text'])
+            em1.set_author(name="Rulings for " + prettify_name(data['card_name']) + " Part 1")
+            em2.set_author(name="Rulings for " + prettify_name(data['card_name']) + " Part 2")
             return [em1, em2], None
         else:
-            em = discord.Embed(title="Rulings for " + prettify_name(data['card_name']), description='Rulings',
+            em = discord.Embed(title=TITLE_TEXT, description=DESCRIPTION_TEXT,
                                colour=0xDEADBF, url=data['url'])
             for entry in data['entries']:
-                em.add_field(name=entry['source'], value=entry['text'])
-            em.set_author(name="Miya Herald")
+                em.add_field(name=(entry['link'] if 'link' in entry else entry['source']), value=entry['text'])
+            em.set_author(name="Rulings for " + prettify_name(data['card_name']))
             return [em], None
     else:
         message = ""
@@ -73,25 +75,29 @@ def split_rulings(entries):
     cleaned = []
     char_count = 0
     split_embed_at = 0
+    count = 1
     for entry in entries:
-        char_count += len(entry['source']) + len(entry['text'])
+        entry['text'] = entry['text'] + ("\n[" + entry['source'] + "](" + entry['link'] + ")" if 'link' in entry else '')
+        char_count += len(str(count)) + len(entry['text'])
         if len(entry['text']) > 1024:
             text = entry['text']
-            count = 1
+            first = True
             while len(text) > 1024:
                 split_point = text[0:1023].rfind('\n\n')
-                cleaned.append({'source': entry['source'] + " Part " + str(count),
+                cleaned.append({'source': ("#" + str(count) if first else "-"),
                                 'text': entry['text'][0:split_point]})
                 text = text[split_point:]
-                count += 1
-                char_count += len(entry['source']) + len(" Part " + str(count))
-            cleaned.append({'source': entry['source'] + (" Part " + str(count)) if count > 1 else '',
+                char_count += 1 if not first else 0
+                first = False
+            char_count += 1
+            cleaned.append({'source': "-",
                             'text': text})
         else:
-            cleaned.append({'source': entry['source'],
+            cleaned.append({'source': "#" + str(count),
                             'text': entry['text']})
         if char_count > 5500:
             split_embed_at = entries.index(entry)
             char_count = 0
+        count += 1
 
     return cleaned, split_embed_at
